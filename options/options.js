db5fc17f4354e20b44b60ff4d37efe683930b515
Crowdbotics <team@crowdbotics.com>
2021-08-19 21:39:03 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "soft_recipe_29958Identifier",
+  url: "https://soft_recipe_29958Identifier.botics.co",
+  api: "https://soft_recipe_29958Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
